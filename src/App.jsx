77a49fd07db892471f251421f35e635d00f6404a
raigumieri <rai.gumiere@hotmail.com>
2023-10-10 09:52:04 -0300

    Projeto Completo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import TextSize from "./Components/TextSize"
+import TextPerson from "./Components/TextPerson"
+import TextColor from "./Components/TextColor"
+import TextBackground from "./Components/TextBackground"
+import Images from "./Components/Images"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <TextSize />
+        <TextPerson />
+        <TextColor />
+        <TextBackground />
+        <Images />
+      </div>
+    </>
+  )
+}
+
+export default App
